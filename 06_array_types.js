cff7312642e4e95af3663dec12b3c138fc4d2ec2
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-07-28 17:12:19 +0300

    Array referans tipi

---
+++ 06_array_types.js
@@ -0,0 +1,14 @@
+// Reference Types: Array (Basics)
+
+let names = ['Eren', 'Mustafa', 'Sultan', 'Zeynep'];
+
+console.log(typeof names);  // object
+console.log(names);
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);  // undefined
+
+// Javascript Array fonksiyon ve özelliklerinden length 
+console.log(names.length);  // 4
