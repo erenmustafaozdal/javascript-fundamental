56eb24bf4d03f75c30c448258e2cb03153b0c051
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-07-28 17:02:07 +0300

    Veri tipleri

---
+++ 04_data_types.js
@@ -0,0 +1,14 @@
+// Data Types
+
+let firstName = 'Eren';
+// String veri tipleri diğer tırnak çeşitleri ile de tanımlanabilir: "Eren" veya `Eren`
+let age = 38;
+let isMaried = true;
+let languages;  // veya let languages = undefined;
+let language = null;
+
+console.log(typeof firstName);  // string
+console.log(typeof age);  // number
+console.log(typeof isMaried);  // boolean
+console.log(typeof languages);  // undefined
+console.log(typeof language);  // object
