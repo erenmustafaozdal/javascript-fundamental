98498bb877ac4a8c56fe6edec0f08f74a1e2940d
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-07-28 17:08:00 +0300

    Object referans tipi

---
+++ 05_object_types.js
@@ -0,0 +1,16 @@
+// Reference Types: Object (Basics)
+
+let user = {
+    name: 'Eren',
+    age: 38,
+    language: 'Türkçe',
+};
+
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+console.log(user.language);
+
+console.log(user['name']);
+console.log(user['age']);
+console.log(user['language']);
