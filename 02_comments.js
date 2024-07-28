726d1d2fa91a671051e747c577efdaf9e2512a7f
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-07-28 16:37:37 +0300

    Yorum satırları

---
+++ 02_comments.js
@@ -0,0 +1,9 @@
+console.log("Bu yazı konsola yazdırılacak.");
+// console.log("Bu yazı tek satırlık yorum olduğu için konsola yazdırılmayacak.");
+
+/*
+console.log("Bu yazılar çok satırlı yorum satırları olduğu için");
+console.log("ekrana yazdırılmayacak.");
+*/
+
+console.log("Bu yazı konsola yazdırılacak.");
