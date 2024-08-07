31b945bceb5a0285a689fdb87ccc4673c68ddde8
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 11:41:56 +0300

    Atama operatörleri

---
+++ 02_operators/02_assignment_operators.js
@@ -0,0 +1,37 @@
+// Assignment Operators
+
+let num1 = 10;
+let num2 = num1;  // num2 = 10;
+
+console.log(num1, num2);
+
+num1 = num1 + 5;
+console.log(num1);  // 15
+num1 += 5;  // num1 = num1 + 5;
+console.log(num1);  // 20
+
+num1 = num1 - 5;
+console.log(num1);  // 15
+num1 -= 5;  // num1 = num1 - 5;
+console.log(num1);  // 10
+
+num1 = num1 * 5;
+console.log(num1);  // 50
+num1 *= 5;  // num1 = num1 * 5;
+console.log(num1);  // 250
+console.log(num1);  // 10
+
+num1 = num1 / 5;
+console.log(num1);  // 50
+num1 /= 5;  // num1 = num1 / 5;
+console.log(num1);  // 10
+
+num1 = num1 + 1;
+console.log(num1);  // 11
+num1++;  // num1 = num1 + 1;
+console.log(num1);  // 12
+
+num1 = num1 - 1;
+console.log(num1);  // 11
+num1--;  // num1 = num1 - 1;
+console.log(num1);  // 10
