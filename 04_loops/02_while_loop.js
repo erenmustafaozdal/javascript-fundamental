7317308ff7fcde30277e99fc5b7d3a0c5363d38b
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 17:31:03 +0300

    While döngüsü

---
+++ 04_loops/02_while_loop.js
@@ -0,0 +1,11 @@
+// While Loop
+
+for (let index = 1; index <= 4; index++) {
+    console.log('Selam Eren', index);
+}
+
+let index = 1;
+while (index <= 4) {
+    console.log('Selam Eren', index);
+    index++;
+}
