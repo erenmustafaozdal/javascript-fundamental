03a82fb3b8ca4a7d613072b98f741a0c532a44c7
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 11:49:45 +0300

    Karşılaştırma operatörleri

---
+++ 02_operators/03_comparison_operators.js
@@ -0,0 +1,23 @@
+// Comparison Operators
+
+/**
+ * <: küçüktür
+ * >: büyüktür
+ * <=: küçük eşittir
+ * >=: büyük eşittir
+ * ==: eşittir (değer karşılaştırması)
+ * ===: eşittir (değer ve tip karşılaştırması)
+ * !=: eşit değildir (değer karşılaştırması)
+ * !==: eşit değildir (değer ve tip karşılaştırması)
+ */
+
+let price = 200;
+
+console.log(price > 300);  // false
+console.log(price >= 200);  // true
+console.log(price < 100);  // false
+console.log(price <= 500);  // true
+console.log(price == '200');  // true
+console.log(price === '200');  // false
+console.log(price != '200');  // false
+console.log(price !== '200');  // true
