883e946c46bf3bc1bdc70d4e0c21b464d1592609
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 16:45:29 +0300

    If else şartları

---
+++ 03_conditional_statements/01_if_else.js
@@ -0,0 +1,21 @@
+// If Else
+
+// if (condition1) {
+//     statements
+// } else if (condition2) {
+//     statements
+// } else if (condition3) {
+//     statements
+// } else {
+//     statements
+// }
+
+let number = 0;
+
+if (number > 0) {
+    console.log('Pozitif bir sayı');
+} else if (number < 0) {
+    console.log('Negatif bir sayı');
+} else {
+    console.log('Sayı sıfıra eşit');
+}
