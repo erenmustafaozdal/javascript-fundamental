ae9b93a83e028101b0e31cd27ddc5d7f75636df0
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 18:07:32 +0300

    String temelleri

---
+++ 05_strings/01_string_basics.js
@@ -0,0 +1,20 @@
+// String Basics
+
+let country = 'Türkiye';
+console.log(country);
+console.log(typeof country);
+console.log(country[0]);
+console.log(country[1]);
+console.log(country[2]);
+console.log(country[3]);
+console.log(country[4]);
+console.log(country[5]);
+console.log(country[6]);
+
+console.log(country.charAt(0));
+console.log(country.charAt(1));
+console.log(country.charAt(2));
+console.log(country.charAt(3));
+console.log(country.charAt(4));
+console.log(country.charAt(5));
+console.log(country.charAt(6));
