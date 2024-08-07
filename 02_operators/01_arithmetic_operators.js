f9d768fb6b3f94e4fcf619a590824c2561cd360b
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 11:35:54 +0300

    Aritmetik operatörler

---
+++ 02_operators/01_arithmetic_operators.js
@@ -0,0 +1,12 @@
+// Arithmetic Operators
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2);  // Toplama
+console.log(num1 - num2);  // Çıkarma
+console.log(num1 * num2);  // Çarpma
+console.log(num1 / num2);  // Bölme
+
+console.log(num1 ** num2);  // Üs alma
+console.log(num1 % num2);  // Mod alma
