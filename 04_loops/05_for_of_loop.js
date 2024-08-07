e43f76b519663662814572b022192d06d0802bcb
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 17:40:35 +0300

    For of döngüsü

---
+++ 04_loops/05_for_of_loop.js
@@ -0,0 +1,11 @@
+// For Of Loop
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let index in numbers) {
+    console.log(numbers[index]);
+}
+
+for (let number of numbers) {
+    console.log(number);
+}
