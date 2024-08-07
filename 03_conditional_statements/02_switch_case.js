032e4e9eb43855910904b64467f29eb4c303deaf
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 17:03:23 +0300

    Switch case şartları

---
+++ 03_conditional_statements/02_switch_case.js
@@ -0,0 +1,15 @@
+// Switch Case
+
+let color = 'black';
+
+switch (color) {
+    case 'black':
+        console.log('Renk siyahtır');
+        break;
+    case 'yellow':
+        console.log('Renk sarıdır');
+        break;
+    case 'red':
+        console.log('Renk kırmızıdır');
+        break;
+}
