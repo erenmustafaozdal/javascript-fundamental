eee5b1dfbd42bef7fb8138b2b020173c80747319
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 17:07:53 +0300

    Tek satırlık üçlü (ternary) operatörler

---
+++ 03_conditional_statements/03_ternary_operators.js
@@ -0,0 +1,20 @@
+// Ternary Operators
+
+let number1 = 10;
+let number2 = 2;
+
+// Uzun yoldan yapılması
+let maxValue;
+
+if (number1 > number2) {
+    maxValue = number1;
+} else {
+    maxValue = number2;
+}
+
+console.log(maxValue);
+
+
+// Tek satırla yapılması
+let maxValue2 = number1 > number2 ? number1 : number2;
+console.log(maxValue2);
