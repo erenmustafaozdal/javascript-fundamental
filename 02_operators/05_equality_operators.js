998169727e47abf3a267af740fa5f4e891a65941
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 16:32:15 +0300

    Eşitlik operatörleri

---
+++ 02_operators/05_equality_operators.js
@@ -0,0 +1,9 @@
+// Equality Operators
+
+let price = 10;
+
+console.log(price === '10');  // false
+console.log(price !== '10');  // true
+
+console.log(price == '10');  // true
+console.log(price != '10');  // false
