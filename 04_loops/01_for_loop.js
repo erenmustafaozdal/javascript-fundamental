923be178d86aac579a121e597cef97fae051f37b
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 17:16:49 +0300

    For döngüsü

---
+++ 04_loops/01_for_loop.js
@@ -0,0 +1,14 @@
+// For Loop
+
+console.log('Merhaba Eren');
+console.log('Merhaba Eren');
+console.log('Merhaba Eren');
+console.log('Merhaba Eren');
+
+for (let index = 1; index <= 4; index++) {
+    console.log('Selam Eren', index);
+}
+
+for (let index = 4; index >= 0; index--) {
+    console.log('Selam Eren', index);
+}
