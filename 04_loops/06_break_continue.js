4ab3b90cf1058ca1cac92b007fed3ff59b182122
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 17:43:26 +0300

    Break ve continue ifadeleri

---
+++ 04_loops/06_break_continue.js
@@ -0,0 +1,15 @@
+// Break & Continue
+
+for (let index = 1; index <= 20; index++) {
+
+    // continue: ara verir
+    if (index === 10) {
+        continue;
+    }
+
+    // break: döngüyü kırar
+    if (index === 15) {
+        break;
+    }
+    console.log(index);
+}
