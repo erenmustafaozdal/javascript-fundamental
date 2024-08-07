7d1ede8294a03d76e652126caf03cb76759f589c
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-07 11:11:20 +0300

    Template literals

---
+++ 08_template_literals.js
@@ -0,0 +1,13 @@
+// Template Literals
+
+let firstName = 'Eren';
+console.log('Benim adım ' + firstName);  // Benim adım Eren
+console.log(`Benim adım ${firstName}`);  // Benim adım Eren
+
+console.log(`${20 + 30}`);
+
+let message = 'Benim adım \nEren Mustafa Özdal';
+let message2 = `Benim adım
+Eren Mustafa Özdal`;
+console.log(message);
+console.log(message2);
