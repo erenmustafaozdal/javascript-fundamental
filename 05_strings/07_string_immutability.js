4c9ec2e1424a723079bf38d2cc84ea9c1d88f999
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-11 19:27:17 +0300

    String verilerinin değiştirilemezliği

---
+++ 05_strings/07_string_immutability.js
@@ -0,0 +1,8 @@
+// String Immutability
+
+// Bir stringin oluşturulduktan sonra içerdiği karakterler doğrudan değiştirilemez
+let message = 'Tarhana Eren';
+message[0] = 'M';  // değişiklik yapmaz
+message[1] = 'e';  // değişiklik yapmaz
+message[6] = 'b';  // değişiklik yapmaz
+console.log(message);
