16af7bee8c611abd1e5fa4d4004fd897caf81b2d
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-11 19:11:19 +0300

    Kaçış dizeleri

---
+++ 05_strings/06_escape_sequences.js
@@ -0,0 +1,17 @@
+// Escape Sequences
+
+// Özel karakterlerden kaçınmak için \ kullanılır
+let message = 'Sultan ve Zeynep "İstanbul\'dan" gelecek.';  // \' = '     \" = "
+console.log(message);
+let path = 'C:\\Users\\usern_name\\code\\javascript-projects\\javascript-fundamental';  // \\=\
+console.log(path);
+let newLine = 'Bu cümle\nburadan yeni satıra yazılır.';  // \n = yeni satır
+console.log(newLine);
+let line1 = 'İsim\tYaş\tRenk';
+let line2 = 'Eren\t38\tYeşil';
+let line3 = 'Sultan\t38\tMavi';
+let line4 = 'Zeynep\t7\tMor';
+console.log(line1);
+console.log(line2);
+console.log(line3);
+console.log(line4);
