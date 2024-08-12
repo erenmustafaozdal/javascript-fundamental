59b181ada65c9ff6920e09805a396fee1a07b459
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-08-12 15:19:55 +0300

    Javascript nesnelerinin temelleri

---
+++ 06_objects/01_object_basics.js
@@ -0,0 +1,19 @@
+// Javascript Object Basics
+
+let firstName = 'Eren';
+let age = 38;
+let hobby = 'Kitap okumak';
+
+const user = {
+    firstName: 'Eren',
+    age: 38,
+    hobby: 'Kitap okumak',
+};
+
+console.log(user);
+console.log(user.firstName);
+console.log(user.age);
+console.log(user.hobby);
+console.log(user['firstName']);
+console.log(user['age']);
+console.log(user['hobby']);
